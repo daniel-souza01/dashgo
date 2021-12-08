8887a41c3c3fc51a88b61c7e15d8ea7a34963257
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2021-12-08 13:20:54 -0300

    Dashboard responsivo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -68,7 +68,7 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritons da semana
             </Text>
