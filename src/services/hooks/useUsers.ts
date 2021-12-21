ad5a93c8089ac0f6fc6b9a3501074079032b9618
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2021-12-21 16:30:31 -0300

    SSR no React Query

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useQuery, UseQueryOptions, UseQueryResult } from 'react-query'
 import { api } from '../api'
 
 type User = {
@@ -38,8 +38,9 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   return { users, totalCount }
 }
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options: UseQueryOptions) {
   return useQuery(['users', page], () => getUsers(page), {
-    staleTime: 1000 * 60 * 10 // 10 minutes
-  })
+    staleTime: 1000 * 60 * 10, // 10 minutes
+    ...options
+  }) as UseQueryResult<GetUsersResponse, unknown>
 }
